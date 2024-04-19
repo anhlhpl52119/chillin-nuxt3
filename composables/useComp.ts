345e02902ced4293f109d318cbfadb0f385376fe
Hung Anh <anhlhpl52119@gmail.com>
2024-04-19 20:59:06 +0700

    chore: update

--- composables/useComp.ts
+++ composables/useComp.ts
@@ -2,6 +2,6 @@ export function useComp(){
     const helloWorld = reactive({controller: 'nothing'});
 
     watch(()=> [helloWorld], () => {
-        console.log('hello');
+        console.log('');
     })
 }
