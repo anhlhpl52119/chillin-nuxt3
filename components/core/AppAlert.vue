bfbed4b91b88d339498fd61e4cc37ac09a0ef1fb
Hung Anh <anhlhpl52119@gmail.com>
2024-04-21 21:30:35 +0700

    refactor: naming component

---
+++ components/core/AppAlert.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    This is app AppAlert
+    <p v-if="foo">this is foo</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface newItem {
+  foo: string;
+  item: string
+}
+defineProps({
+  foo: {
+    type: [Boolean, String],
+    default: true
+  }
+})
+</script>
