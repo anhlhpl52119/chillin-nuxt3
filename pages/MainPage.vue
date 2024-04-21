3a32873de27e4201e9d2e0e5158c79ab8c2302d0
Hung Anh <anhlhpl52119@gmail.com>
2024-04-21 21:30:40 +0700

    feat: init

---
+++ pages/MainPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <main style="background-color: #2c2a2a; color: white; height: 90vh; padding: 30px">
+
+  <div>
+    this is main page
+  </div>
+
+  <div>
+    <AppAlert></AppAlert>
+  </div>
+</main>
+
+</template>
+
+<script lang="ts" setup>
+import AppAlert from '@/components/core/AppAlert.vue';
+definePageMeta({
+    name: 'Test',
+    path: '/',
+})
+</script>
